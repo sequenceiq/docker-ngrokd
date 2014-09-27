e99cac10381108653b4fcff2a06fa4e33c3d4c9b
lalyos <lalyos@yahoo.com>
2014-09-27 02:25:37 +0200

    Fix typo in linux binary name

--- deploy-binaries.sh
+++ deploy-binaries.sh
@@ -4,10 +4,10 @@
 
 docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/darwin_amd64/ngrok . && mv ngrok ngrok_darwin
 docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/darwin_amd64/ngrokd . && mv ngrokd ngrokd_darwin
-docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/ngrok . && mv ngrok ngrok_liux
-docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/ngrokd . && mv ngrokd ngrokd_liux
+docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/ngrok . && mv ngrok ngrok_linux
+docker cp $CONTAINER:/go/src/github.com/inconshreveable/ngrok/bin/ngrokd . && mv ngrokd ngrokd_linux
 
-UPLOADS="ngrok_darwin ngrokd_darwin ngrok_liux ngrokd_liux"
+UPLOADS="ngrok_darwin ngrokd_darwin ngrok_linux ngrokd_linux"
 for file in $UPLOADS;
   do echo $file
   aws s3 cp $file s3://sequenceiq
